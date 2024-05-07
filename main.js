33b0d715000f26c976a7dfc2fed498345eec4d7a
PabloFrancone <franconepablo@gmail.com>
2024-05-07 18:44:46 -0300

    cambio 2 (crear cielo)

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [Game],
 };
 
 // Create a new Phaser game instance
