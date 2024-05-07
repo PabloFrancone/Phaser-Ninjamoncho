f1d50e3f98533a706fdff92603c1425e7a370c35
PabloFrancone <franconepablo@gmail.com>
2024-05-07 20:33:26 -0300

    Camios 3

--- scenes/Game.js
+++ scenes/Game.js
@@ -13,17 +13,55 @@ export default class Game extends Phaser.Scene {
     //cargar assets
   
     //import cielo
-    this.load.image("Cielo","../public/assets/Cielo.webp");
+    this.load.image("cielo","../public/assets/Cielo.webp");
+
+    //import plataforma
+    this.load.image("plataforma","../public/assets/platform.png");
+   
+    //import ninja
+    this.load.image("ninja","../public/assets/Ninja.png");
   }
 
   create() {
     // crear elementos
-    const Cielo = this.add.image(400,300,"Cielo")
+
+    this.Cielo = this.add.image(400,300,"cielo");
+    this.Cielo.setScale(2);
+
+    this.plataformas = this.physics.add.staticGroup();
+    this.plataformas.create(400, 568, "plataforma").setScale(2).refreshBody();
+    this.plataformas.create(200, 200, "plataforma")
+
+
+    this.ninja = this.physics.add.sprite(400,300,"ninja");
+    this.ninja.setScale(0.1);
+    this.ninja.setCollideWorldBounds(true);
+
+    //agrupar colision entre personaje y plataforma
+
+     this.physics.add.collider(this.ninja, this.plataformas);
+
+     //crear teclas 
+     this.cursor = this.input.keyboard.createCursorKeys();
+
+     //letra por letra
+     //this.cursor = this.input.keyboard.addkey()
+
 
   }
  
 
   update() {
-
+//movimiento personaje
+    if(this.cursor.left.isDown){
+      this.ninja.setVelocityX(-160);
+    }else if(this.cursor.right.isDown){
+      this.ninja.setVelocityX(160);
+    }else{
+      this.ninja.setVelocityX(0);
+    }
+    if (this.cursor.up.isDown && this.ninja.body.tounching.down){
+      this.ninja.setVelocityY(-330);
+    }
   }
 }
