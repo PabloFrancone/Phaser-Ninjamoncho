2cb0837de5b999e6dba5fbfa35e6fa2726250060
PabloFrancone <franconepablo@gmail.com>
2024-05-28 18:47:04 -0300

    cambios 5

--- scenes/Game.js
+++ scenes/Game.js
@@ -5,6 +5,14 @@ export default class Game extends Phaser.Scene {
   }
 
   init() { 
+    this.score = 0;  
+    this.shapes = {
+   "triangulo": {points: 10, count: 0},
+   "cuadrado": {points: 20, count: 0}, 
+   "rombo": {points: 30, count: 0}, 
+
+  this:checWin(),
+  };
    
   }
 
@@ -23,10 +31,30 @@ export default class Game extends Phaser.Scene {
     //recolectables
     this.load.image("roca","../public/assets/roca1.webp");
     this.load.image("araña","../public/assets/araña1.png");
+    this.load.image("omni","../public/assets/platoVolador.png");
+
 
   }
 
   create() {
+    checwin
+  this.scoreText = setText(16, 60, 
+  `puntaje: ${this .score} 
+   T: ${tis.shapes["roca"]}.count}  
+   C: ${tis.shapes["araña"]}.count}  
+   H: ${tis.shapes["omni"]}.count}`
+   );
+   
+   const cumplepuntos = this.score >= 100;
+   const cumplefiguras =
+      this.shapes["roca"].count >= 2 &&
+      this.shapes["araña"].count >= 2 &&
+      this.shapes["omni"].count >= 2 &&
+   
+   if (cumplepuntos && cumplefiguras) {
+       console.log("ganaste");
+   }
+   
     // crear elementos
 
     this.cielo = this.add.image(400,300,"cielo");
@@ -76,20 +104,11 @@ export default class Game extends Phaser.Scene {
   });
   }
  
- onSecond(){
-  //creo recolectable
-  const tipos = ["araña", "roca"];
-  const tipo = Phaser.Math.RND.pick(tipos); 
-  console.log(tipo)
-  let recolectable = this.recolectables.create(
-    Phaser.Math.Between(10, 790),
-    0,
-    tipo
-  ).setScale(0.2 )
+
 
  }
 
-  update() {
+  update() ;
 //movimiento personaje
     if(this.cursor.left.isDown){
       this.ninja.setVelocityX(-160);
@@ -101,5 +120,32 @@ export default class Game extends Phaser.Scene {
     if (this.cursor.up.isDown && this.ninja.body.touching.down){
       this.ninja.setVelocityY(-330);
     }
-  }
-}
+  
+
+ onSecond();
+  //creo recolectable
+  const tipos = ["araña", "roca", "omni"];
+  const tipo = Phaser.Math.RND.pick(tipos); 
+  console.log(tipo)
+  let recolectable = this.recolectables.create(
+    Phaser.Math.Between(10, 790),
+    0,
+    tipo
+  ).setScale(0.2 )
+  
+
+
+  onshapecollect(personaje, recolectoable) ;
+    console.log("recolectado", recolectable.texture.key);
+    const nobreFig = recolectable.texture.key;
+    
+    this.score += this.shapes[nombreFig].points;
+    this.shapes[nomberFig].count += 1;
+    
+    console.table(this.shapes);
+    console.log("score", this.score);
+    recolectable.destroy();
+
+    
+    
+      
